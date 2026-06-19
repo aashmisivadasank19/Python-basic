1c5b305963e8b0889ca3f3ac0a8690c57be31777
K Aashmi Sivadasan <aashmisivadasank@gmail.com>
2026-06-19 08:46:54 +0530

    Add files via upload

---
+++ celsius to fa.py
@@ -0,0 +1,6 @@
+c=float(input('Enter temperature in celsius'))
+fahrenheit=(c*9/5)+32
+print("Temperature in fahrenheit is:", fahrenheit)
+fahrenheit=float(input("Enter temperature in fahrenheit: "))
+celsius=(fahrenheit-32)*5/9
+print("Temperature in celsius is:", celsius)
