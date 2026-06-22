0f0855582a7e49f8f84a31355be86a2aab9d98aa
K Aashmi Sivadasan <aashmisivadasank@gmail.com>
2026-06-22 22:46:03 +0530

    Add files via upload

---
+++ Kilometers to miles.py
@@ -0,0 +1,4 @@
+Kilometers = float(input("Enter distance in kilometers:"))
+conversion_factor = 0.621371
+miles = kilometers * conversion_factor
+print("{} kilometers is equal to {} miles".format(Kilometers, miles))
