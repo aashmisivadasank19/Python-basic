ef84d6a064d3b490aa9558acb08da2f00ccca5bd
K Aashmi Sivadasan <aashmisivadasank@gmail.com>
2026-07-27 22:50:48 +0530

    Add files via upload

---
+++ delimiter.py
@@ -0,0 +1,12 @@
+s = 'pining for the fjords'
+t = s.split()
+print(t)
+
+
+s = 'spam-spam-spam'
+delimiter = '-'
+s.split(delimiter)
+
+t = ['pining', 'for', 'the', 'fjords']
+delimiter = ' '
+delimiter.join(t)
